a2d0c12d3d1c6e72662f203d16e7ad12f90b0aa3
Om-Sureja <omsureja@gmail.com>
2024-06-13 03:00:13 +0530

    3-express_with_zod

---
+++ 3-express_with_zod/dist/tests/sum.test.js
@@ -0,0 +1,44 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const globals_1 = require("@jest/globals");
+const supertest_1 = __importDefault(require("supertest"));
+const index_1 = require("../index");
+(0, globals_1.describe)("POST /sum", () => {
+    (0, globals_1.it)("it should add 1 and 2 to get 3", () => __awaiter(void 0, void 0, void 0, function* () {
+        const res = yield (0, supertest_1.default)(index_1.app).post("/sum").send({
+            a: 1,
+            b: 2
+        });
+        (0, globals_1.expect)(res.statusCode).toBe(200);
+        (0, globals_1.expect)(res.body.ans).toBe(3);
+    }));
+    (0, globals_1.it)("should return error for wrong/empty outputs", () => __awaiter(void 0, void 0, void 0, function* () {
+        const res = yield (0, supertest_1.default)(index_1.app).post("/sum").send({});
+        (0, globals_1.expect)(res.statusCode).toBe(411);
+        (0, globals_1.expect)(res.body.message).toBe("Incorrect outputs");
+    }));
+});
+(0, globals_1.describe)("GET /sum", () => {
+    (0, globals_1.it)("it should return 1 and 2 as 3", () => __awaiter(void 0, void 0, void 0, function* () {
+        const res = yield (0, supertest_1.default)(index_1.app).get("/sum").set({ a: "1", b: "2" }).send();
+        (0, globals_1.expect)(res.statusCode).toBe(200);
+        (0, globals_1.expect)(res.body.ans).toBe(3);
+    }));
+    (0, globals_1.it)("it should return 411 for wrong/empty outputs", () => __awaiter(void 0, void 0, void 0, function* () {
+        const res = yield (0, supertest_1.default)(index_1.app).get("/sum").set({}).send();
+        (0, globals_1.expect)(res.statusCode).toBe(411);
+        (0, globals_1.expect)(res.body.message).toBe("No headers found");
+    }));
+});
